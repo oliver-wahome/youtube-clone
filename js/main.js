1f2d0323b2b447378ce5e59228d8b159a4f3b0a0
oliver-wahome <olivernderitu135@gmail.com>
2022-11-21 23:58:22 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+//links video from home page to video page
+document.getElementById("video1").onclick = function(){
+    window.location.href = "video.html";
+}
+
+//links video from home page to video page
+document.getElementById("video2").onclick = function(){
+    window.location.href = "video.html";
+}
+
+//links video from home page to video page
+document.getElementById("video3").onclick = function(){
+    window.location.href = "video.html";
+}
+
+//links video from home page to video page
+document.getElementById("video4").onclick = function(){
+    window.location.href = "video.html";
+}
+
+//links video from home page to video page
+document.getElementById("video5").onclick = function(){
+    window.location.href = "video.html";
+}
+
+//Icon link to home page on navbar
+document.getElementById("icon").onclick = function(){
+    window.location.href = "index.html";
+}
+
+
