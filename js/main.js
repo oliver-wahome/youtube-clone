c30478db01c9777a138c70af2bbc61735805e960
oliver-wahome <olivernderitu135@gmail.com>
2022-11-28 16:49:53 +0300

    Javascript refactoring complete

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-//Adding video object data
+//Adding video object data to array
 //In a real life application I would get this data from a database
-var video1 = {
-    id: 1,
+var videoData = [];
+videoData[0] = {
+    id: 0,
     source: "https://player.vimeo.com/external/347925811.sd.mp4?s=e48915e5abacccf4471e3daf61611fd8af52ca87&profile_id=164&oauth2_token_id=57447761",
     title: "Manchester United vs Chelsea | Match Highlights",
     channel: "DailyFootball",
@@ -12,8 +13,8 @@ var video1 = {
     description: "Full match hightlights of the game between Manchester United and Chelsea at Stamford Bridge. I hope you enjoy."
 }
 
-var video2 = {
-    id: 2,
+videoData[1] = {
+    id: 1,
     source: "https://player.vimeo.com/external/392681120.sd.mp4?s=7c032f96adf30f26162fbec0cf91f14753aa5b9d&profile_id=164&oauth2_token_id=57447761",
     title: "Wildlife Photography at Nairobi National Park",
     channel: "The Seasoned Traveler",
@@ -24,8 +25,8 @@ var video2 = {
     description: "This is some footage from when I was in Nairobi National Park on a safari with my family."
 }
 
-var video3 = {
-    id: 3,
+videoData[2] = {
+    id: 2,
     source: "https://player.vimeo.com/external/372182116.sd.mp4?s=2225f7b0666c1baeb2dd4e0580c43c1a0bab6920&profile_id=164&oauth2_token_id=57447761",
     title: "A Day in the life of a Jumia Product Manager",
     channel: "Jane Akinyi",
@@ -36,8 +37,8 @@ var video3 = {
     description: "Hi I'm Jane Akinyi a product manager at Jumia Kenya and this is my day in the life video."
 }
 
-var video4 = {
-    id: 4,
+videoData[3] = {
+    id: 3,
     source: "https://player.vimeo.com/external/398510938.sd.mp4?s=d52c2d502a2e3a8246b540a1b1ca97916661dcb7&profile_id=164&oauth2_token_id=57447761",
     title: "Walter White On Lunch Break | \"Breaking Bread\"",
     channel: "BreakingBadStan",
@@ -48,8 +49,8 @@ var video4 = {
     description: "This is what I imagine Walter White would do during his lunch break at Gustavo's meth factory."
 }
 
-var video5 = {
-    id: 5,
+videoData[4] = {
+    id: 4,
     source: "https://player.vimeo.com/external/260569255.sd.mp4?s=748317eb87cd75ee7a93ff0fd8b0711759f2196a&profile_id=164&oauth2_token_id=57447761",
     title: "WhatsApp is Tracking You!!",
     channel: "Tech Insider",
@@ -60,70 +61,19 @@ var video5 = {
     description: "'WhatsApp is Tracking You!' were the first words that came out of John Doe's mouth. John Doe is an ex WhatsApp cybersecurity engineer who provided us with this chilling information."
 }
 
-
-var url = window.location.href;
-var urlString = url.substring(27);
-
-//loading video object data to index.html
-//video1
-document.getElementById("video1").src = video1.source;
-document.getElementById("video1Title").innerText = video1.title;
-document.getElementById("video1ChannelIcon").src = video1.channelIcon;
-document.getElementById("video1ChannelName").innerText = video1.channel;
-document.getElementById("video1Views").innerText = video1.views + " views";
-document.getElementById("video1TimeOnline").innerText = video1.timeOnline + " ago";
-//video2
-document.getElementById("video2").src = video2.source;
-document.getElementById("video2Title").innerText = video2.title;
-document.getElementById("video2ChannelIcon").src = video2.channelIcon;
-document.getElementById("video2ChannelName").innerText = video2.channel;
-document.getElementById("video2Views").innerText = video2.views + " views";
-document.getElementById("video2TimeOnline").innerText = video2.timeOnline + " ago";
-//video3
-document.getElementById("video3").src = video3.source;
-document.getElementById("video3Title").innerText = video3.title;
-document.getElementById("video3ChannelIcon").src = video3.channelIcon;
-document.getElementById("video3ChannelName").innerText = video3.channel;
-document.getElementById("video3Views").innerText = video3.views + " views";
-document.getElementById("video3TimeOnline").innerText = video3.timeOnline + " ago";
-//video4
-document.getElementById("video4").src = video4.source;
-document.getElementById("video4Title").innerText = video4.title;
-document.getElementById("video4ChannelIcon").src = video4.channelIcon;
-document.getElementById("video4ChannelName").innerText = video4.channel;
-document.getElementById("video4Views").innerText = video4.views + " views";
-document.getElementById("video4TimeOnline").innerText = video4.timeOnline + " ago";
-//video5
-document.getElementById("video5").src = video5.source;
-document.getElementById("video5Title").innerText = video5.title;
-document.getElementById("video5ChannelIcon").src = video5.channelIcon;
-document.getElementById("video5ChannelName").innerText = video5.channel;
-document.getElementById("video5Views").innerText = video5.views + " views";
-document.getElementById("video5TimeOnline").innerText = video5.timeOnline + " ago";
-
-//links video from home page to video page
-document.getElementById("video1").onclick = function(){
-    window.location.href = "video.html?" + video1.id;
+//loading video data to their respective elements in index.html
+for(let i=0; i<videoData.length; i++){
+    document.getElementById("video"+(i+1)).src = videoData[i].source;
+    document.getElementById("video"+(i+1)+"Title").innerText = videoData[i].title;
+    document.getElementById("video"+(i+1)+"ChannelIcon").src = videoData[i].channelIcon;
+    document.getElementById("video"+(i+1)+"ChannelName").innerText = videoData[i].channel;
+    document.getElementById("video"+(i+1)+"Views").innerText = videoData[i].views + " views";
+    document.getElementById("video"+(i+1)+"TimeOnline").innerText = videoData[i].timeOnline + " ago";
 }
 
-//links video from home page to video page
-document.getElementById("video2").onclick = function(){
-    window.location.href = "video.html?" + video2.id;
-}
-
-//links video from home page to video page
-document.getElementById("video3").onclick = function(){
-    window.location.href = "video.html?" + video3.id;
-}
-
-//links video from home page to video page
-document.getElementById("video4").onclick = function(){
-    window.location.href = "video.html?" + video4.id;
-}
-
-//links video from home page to video page
-document.getElementById("video5").onclick = function(){
-    window.location.href = "video.html?" + video5.id;
+//function to add videoData id to url onclick of video
+function videoOnclick(num){
+    window.location.href = "video.html?" + videoData[num].id;
 }
 
 //video expand function
--- js/video.js
+++ js/video.js
@@ -1,7 +1,9 @@
-//Adding video object data
+//Adding video object data to array
 //In a real life application I would get this data from a database
-var video1 = {
-    id: 1,
+const videoData = [];
+
+videoData[0] = {
+    id: 0,
     source: "https://player.vimeo.com/external/347925811.sd.mp4?s=e48915e5abacccf4471e3daf61611fd8af52ca87&profile_id=164&oauth2_token_id=57447761",
     title: "Manchester United vs Chelsea | Match Highlights",
     channel: "DailyFootball",
@@ -12,8 +14,8 @@ var video1 = {
     description: "Full match hightlights of the game between Manchester United and Chelsea at Stamford Bridge. I hope you enjoy."
 }
 
-var video2 = {
-    id: 2,
+videoData[1] = {
+    id: 1,
     source: "https://player.vimeo.com/external/392681120.sd.mp4?s=7c032f96adf30f26162fbec0cf91f14753aa5b9d&profile_id=164&oauth2_token_id=57447761",
     title: "Wildlife Photography at Nairobi National Park",
     channel: "The Seasoned Traveler",
@@ -24,8 +26,8 @@ var video2 = {
     description: "This is some footage from when I was in Nairobi National Park on a safari with my family."
 }
 
-var video3 = {
-    id: 3,
+videoData[2] = {
+    id: 2,
     source: "https://player.vimeo.com/external/372182116.sd.mp4?s=2225f7b0666c1baeb2dd4e0580c43c1a0bab6920&profile_id=164&oauth2_token_id=57447761",
     title: "A Day in the life of a Jumia Product Manager",
     channel: "Jane Akinyi",
@@ -36,8 +38,8 @@ var video3 = {
     description: "Hi I'm Jane Akinyi a product manager at Jumia Kenya and this is my day in the life video."
 }
 
-var video4 = {
-    id: 4,
+videoData[3] = {
+    id: 3,
     source: "https://player.vimeo.com/external/398510938.sd.mp4?s=d52c2d502a2e3a8246b540a1b1ca97916661dcb7&profile_id=164&oauth2_token_id=57447761",
     title: "Walter White On Lunch Break | \"Breaking Bread\"",
     channel: "BreakingBadStan",
@@ -48,8 +50,8 @@ var video4 = {
     description: "This is what I imagine Walter White would do during his lunch break at Gustavo's meth factory."
 }
 
-var video5 = {
-    id: 5,
+videoData[4] = {
+    id: 4,
     source: "https://player.vimeo.com/external/260569255.sd.mp4?s=748317eb87cd75ee7a93ff0fd8b0711759f2196a&profile_id=164&oauth2_token_id=57447761",
     title: "WhatsApp is Tracking You!!",
     channel: "Tech Insider",
@@ -61,56 +63,17 @@ var video5 = {
 }
 
 var urlString = decodeURIComponent(window.location.search);
-var theId = urlString.substring(1);
+var theId = parseInt(urlString.substring(1));
 
-if(theId == "1"){
-    document.getElementById("largeVideo").src = video1.source;
-    document.getElementById("largeVideoTitle").innerText = video1.title.toUpperCase();
-    document.getElementById("largeVideoChannelIcon").src = video1.channelIcon;
-    document.getElementById("largeVideoChannelName").innerText = video1.channel;
-    document.getElementById("largeVideoChannelSubs").innerText = video1.channelSubs + " subscribers";
-    document.getElementById("largeVideoViews").innerText = video1.views + " views";
-    document.getElementById("largeVideoTimeOnline").innerText = video1.timeOnline + " ago";
-    document.getElementById("largeVideoDesc").innerText = video1.description;
-}
-else if(theId == "2"){
-    document.getElementById("largeVideo").src = video2.source;
-    document.getElementById("largeVideoTitle").innerText = video2.title.toUpperCase();
-    document.getElementById("largeVideoChannelIcon").src = video2.channelIcon;
-    document.getElementById("largeVideoChannelName").innerText = video2.channel;
-    document.getElementById("largeVideoChannelSubs").innerText = video2.channelSubs + " subscribers";
-    document.getElementById("largeVideoViews").innerText = video2.views + " views";
-    document.getElementById("largeVideoTimeOnline").innerText = video2.timeOnline + " ago";
-    document.getElementById("largeVideoDesc").innerText = video2.description;
-}
-else if(theId == "3"){
-    document.getElementById("largeVideo").src = video3.source;
-    document.getElementById("largeVideoTitle").innerText = video3.title.toUpperCase();
-    document.getElementById("largeVideoChannelIcon").src = video3.channelIcon;
-    document.getElementById("largeVideoChannelName").innerText = video3.channel;
-    document.getElementById("largeVideoChannelSubs").innerText = video3.channelSubs + " subscribers";
-    document.getElementById("largeVideoViews").innerText = video3.views + " views";
-    document.getElementById("largeVideoTimeOnline").innerText = video3.timeOnline + " ago";
-    document.getElementById("largeVideoDesc").innerText = video3.description;
-}
-else if(theId == "4"){
-    document.getElementById("largeVideo").src = video4.source;
-    document.getElementById("largeVideoTitle").innerText = video4.title.toUpperCase();
-    document.getElementById("largeVideoChannelIcon").src = video4.channelIcon;
-    document.getElementById("largeVideoChannelName").innerText = video4.channel;
-    document.getElementById("largeVideoChannelSubs").innerText = video4.channelSubs + " subscribers";
-    document.getElementById("largeVideoViews").innerText = video4.views + " views";
-    document.getElementById("largeVideoTimeOnline").innerText = video4.timeOnline + " ago";
-    document.getElementById("largeVideoDesc").innerText = video4.description;
-}
-else if(theId == "5"){
-    document.getElementById("largeVideo").src = video5.source;
-    document.getElementById("largeVideoTitle").innerText = video5.title.toUpperCase();
-    document.getElementById("largeVideoChannelIcon").src = video5.channelIcon;
-    document.getElementById("largeVideoChannelName").innerText = video5.channel;
-    document.getElementById("largeVideoChannelSubs").innerText = video5.channelSubs + " subscribers";
-    document.getElementById("largeVideoViews").innerText = video5.views + " views";
-    document.getElementById("largeVideoTimeOnline").innerText = video5.timeOnline + " ago";
-    document.getElementById("largeVideoDesc").innerText = video5.description;
-}
+console.log(theId);
+
+//loads the video data to the respective elements
+document.getElementById("largeVideo").src = videoData[theId].source;
+document.getElementById("largeVideoTitle").innerText = videoData[theId].title.toUpperCase();
+document.getElementById("largeVideoChannelIcon").src = videoData[theId].channelIcon;
+document.getElementById("largeVideoChannelName").innerText = videoData[theId].channel;
+document.getElementById("largeVideoChannelSubs").innerText = videoData[theId].channelSubs + " subscribers";
+document.getElementById("largeVideoViews").innerText = videoData[theId].views + " views";
+document.getElementById("largeVideoTimeOnline").innerText = videoData[theId].timeOnline + " ago";
+document.getElementById("largeVideoDesc").innerText = videoData[theId].description;
 
